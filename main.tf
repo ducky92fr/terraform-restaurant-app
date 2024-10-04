51b3a7905b8d11842fabe1a9fa12d6914782fb46
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-04 17:37:32 +0200

    build terraform

--- main.tf
+++ main.tf
@@ -5,10 +5,17 @@ terraform {
       version = "5.46.0"
     }
   }
+   backend "s3" {
+    bucket = "terraform-state-restaurant-app"     # Replace with your S3 bucket name
+    key    = "terraform/my-terraform.tfstate" # Path to store the state file in S3
+    region = "eu-west-3"                # Your AWS region
+  }
 }
 provider "aws" {
   region = "eu-west-3"
 }
+
+
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-045a8ab02aadf4f88"  # Example AMI
