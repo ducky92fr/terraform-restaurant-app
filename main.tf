2fefe8aff6af2820fd99375ff5a4a87a2ba54504
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-07 16:14:49 +0200

    testing

--- main.tf
+++ main.tf
@@ -8,11 +8,28 @@ terraform {
    backend "s3" {
     bucket = "terraform-state-restaurant-app"    
     key    = "terraform/my-terraform.tfstate"
+    dynamodb_table = "terraform-state-lock"  
+    encrypt        = true        
     region = "eu-west-3"        
   }
 }
 provider "aws" {
   region = "eu-west-3"
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-state-lock"
+  }
 }
 
 module "vpc" {
