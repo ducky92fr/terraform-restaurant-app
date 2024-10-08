f11eaf4693eab0021b3ea6b108c4687f7eceb620
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-08 12:23:48 +0200

    testing

--- terraform/localfile.tf
+++
@@ -1,7 +0,0 @@
-resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/../ansible/inventory.yml"
-  content  = templatefile("${path.module}/../ansible/templates/inventory.tpl", {
-    ec2_public_ips = module.ec2.ec2_public_ips
-  })
-   depends_on = [module.ec2]
-}
