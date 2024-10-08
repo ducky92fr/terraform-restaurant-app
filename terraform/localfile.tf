c7354afd148f0d45afef1c291a07669229a7182e
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-08 11:35:31 +0200

    testing

--- terraform/localfile.tf
+++ terraform/localfile.tf
@@ -1,6 +1,6 @@
 resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../ansible/inventory.yml"
   content  = templatefile("${path.module}/../ansible/templates/inventory.tpl", {
-    ec2_public_ips = output.ec2_public_ips
+    ec2_public_ips = module.ec2.ec2_public_ips
   })
 }
